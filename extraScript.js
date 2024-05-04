675d6b2c5bd3ad635e106f7d7dcdd6a0d59b8a0a
corbiano <168668372+corbiano@users.noreply.github.com>
2024-05-04 13:06:54 -0700

    Update extraScript.js

--- extraScript.js
+++ extraScript.js
@@ -12,7 +12,7 @@ function beginSelectedGame(gameType) {
 var gameArea = {
   	canvas : document.createElement("canvas"),
   	start : function() {
-    this.canvas.width = 100%;
+    this.canvas.width = 480;
     this.canvas.height = 270;
    	this.context = this.canvas.getContext("2d");
    	$(`#gameWindow`).append(this.canvas);

67de7d500a8436ada1344a28e0740a05c8bb00a4
corbiano <168668372+corbiano@users.noreply.github.com>
2024-05-04 14:16:03 -0700

    Update extraScript.js

--- extraScript.js
+++ extraScript.js
@@ -43,9 +43,11 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
-  ctx = gameArea.context;
-  ctx.fillStyle = color;
-  ctx.fillRect(this.x, this.y, this.width, this.height);
+  this.update = function(){
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
 
 

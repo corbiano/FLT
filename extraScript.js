ce6e5952d60734e54a7f3426b619d551ca1cefaf
corbiano <168668372+corbiano@users.noreply.github.com>
2024-05-04 15:35:56 -0700

    Update extraScript.js

--- extraScript.js
+++ extraScript.js
@@ -21,8 +21,11 @@ var gameArea = {
    	    this.context = this.canvas.getContext("2d");
    	    $(`#gameWindow`).append(this.canvas);
         this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keypress', function (e) {
+        window.addEventListener('keydown', function (e) {
             gameArea.key = e.keyCode;
+            })
+        window.addEventListener('keyup', function (e) {
+            gameArea.key = false;
             })
     },
     clear : function() {
@@ -80,7 +83,7 @@ function updateGameArea() {
   gameArea.clear();
   Agent.floorCheck();
   gravity();
-  if (gameArea.key == 87) {Agent.jump()};
+  if (gameArea.key && gameArea.key == 87) {Agent.jump()};
   Agent.newPos();
   Agent.update();
 }

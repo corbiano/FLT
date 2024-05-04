5cdb13fef67748abc031310d697c96ac38930e6a
corbiano <168668372+corbiano@users.noreply.github.com>
2024-05-04 11:08:16 -0700

    fill background of canvas

--- extraScript.js
+++ extraScript.js
@@ -4,9 +4,10 @@ function beginSelectedGame(gameType) {
     gameArea.start();
     if(gameType == "test"){
         //Agent = new component(30, 30, "red", 10, 120);
-        console.log("still working");
+        
     }
 }
+
 
 var gameArea = {
   	canvas : document.createElement("canvas"),
@@ -14,6 +15,8 @@ var gameArea = {
     this.canvas.width = 480;
     this.canvas.height = 270;
    	this.context = this.canvas.getContext("2d");
+    this.context.fillStyle = "white";
+    this.context.fillRect(0, 0, canvas.width, canvas.height);
    	$(`#gameWindow`).append(this.canvas);
   	}
 }

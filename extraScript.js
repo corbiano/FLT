7f831e11193cb2d03ab5008260ad8562cacbba50
corbiano <168668372+corbiano@users.noreply.github.com>
2024-05-07 14:49:14 -0700

    Update extraScript.js

--- extraScript.js
+++ extraScript.js
@@ -11,7 +11,8 @@ var mousePressed = false;
 var highScore = 0;
 var lastScore = 0;
 var killedBy = "";
-var wallet = "";
+var wallet = 0;
+var tempDuck = 0;
 
 
 var gameOpen = false;
@@ -45,14 +46,13 @@ function closeGame() {
 	gameStart = false;
 	gameOpen = false;
 	clearInterval(updateGameArea);
-	window.removeEventListener('mousemove', mouseMove, true)
 	window.removeEventListener('keydown', keyDown, true)
 	window.removeEventListener('keyup', keyUp, true)
 	$(`#gameWindow`).remove();
 }
 
 function beginSelectedGame(gameType) {
-	if(!storeOpen){
+	if(gameOpen){
 		highScore = localStorage.getItem("highScore");
 		wallet = localStorage.getItem("currentDucks");
 		gameArea.start();
@@ -60,92 +60,89 @@ function beginSelectedGame(gameType) {
 		$(`#gameArea`).css("margin", "auto");
 		$(`#gameArea`).css("border-radius", "10px");
 		$(`#gameArea`).css("width", "100%");
+		
 		if(gameStart){
-		backgroundColor("black");
-		Score = 0;
-		frame = 0;
-		gameSpeed = 4;
-		Agent = new component(30, 30, "green", ((gameArea.canvas.width / 2) - 15), ((gameArea.canvas.height * 0.75) - 110), "player");
-		Sup = new component(30, 40, "red", gameArea.canvas.width, ((gameArea.canvas.height * 0.75) + 5), "wall");
-		Duck = new component(10, 10, "yellow", gameArea.canvas.width + 300, (gameArea.canvas.height - 50), "collectable");
+			backgroundColor("black");
+			Score = 0;
+			frame = 0;
+			gameSpeed = 4;
+			Agent = new component(30, 30, "green", ((gameArea.canvas.width / 2) - 15), ((gameArea.canvas.height * 0.75) - 110), "player");
+			Sup = new component(30, 40, "red", gameArea.canvas.width, ((gameArea.canvas.height * 0.75) + 5), "wall");
+			Duck = new component(10, 10, "yellow", gameArea.canvas.width + 300, (gameArea.canvas.height - 50), "collectable");
 		
 		}
 	}
 }
 
 function titleScreen() {
-	if(!storeOpen){
-		var canvas = document.getElementById("gameArea");
-		var ctx = canvas.getContext("2d");
-		
-		ctx.globalCompositeOperation = 'destination-under'
-		
-		ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect((canvas.width / 2 - 88), 150, 176, 100);
-		ctx.fillStyle = "#2b2b2b";
-		ctx.fillRect((canvas.width / 2 - 85), 153, 170, 94);
-		
-		ctx.fillStyle = "white";
-		ctx.font = "60px Arial";
-		ctx.fillText("Play", (canvas.width / 2 - 62), 217);
-		
-		ctx.fillStyle = "red";
-		ctx.font = "70px Arial";
-		ctx.fillText("Agent Jump", canvas.width / 2 - 180, 90);
-		
-		ctx.fillStyle = "white";
-		ctx.font = "15px Arial";
-		ctx.fillText("High score: " + String(localStorage.getItem("highScore")), 10, 170);
-		ctx.fillText("Last run: " + String(localStorage.getItem("lastScore")), 10, 200);
-		ctx.fillText("Killed by: " + String(localStorage.getItem("killedBy")), 10, 230);
-		ctx.fillText("Current Ducks: " + String(localStorage.getItem("currentDucks")), 10, 260);
-		
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect((canvas.width - 100), 170, 70, 70);
-		ctx.fillStyle = "#2b2b2b";
-		ctx.fillRect((canvas.width - 97), 173, 64, 64);
-		
-		
-		ctx.fillStyle = "white";
-		ctx.font = "50px Arial";
-		ctx.fillText("W", canvas.width - 87, 222, 146, 70);
-		ctx.fillStyle = "white";
-		ctx.font = "20px Arial";
-		ctx.fillText("START", canvas.width - 95, 160, 146, 70);
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect((canvas.width - 200), 170, 70, 70);
-		ctx.fillStyle = "#2b2b2b";
-		ctx.fillRect((canvas.width - 197), 173, 64, 64);
-		
-		ctx.fillStyle = "white";
-		ctx.font = "50px Arial";
-		ctx.fillText("S", canvas.width - 182, 222, 146, 70);
-		ctx.fillStyle = "white";
-		ctx.font = "20px Arial";
-		ctx.fillText("STORE", canvas.width - 200, 160, 146, 70);
-	}
+	var canvas = document.getElementById("gameArea");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.globalCompositeOperation = 'destination-under'
+	
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	ctx.fillStyle = "white";
+	ctx.fillRect((canvas.width / 2 - 88), 150, 176, 100);
+	ctx.fillStyle = "#2b2b2b";
+	ctx.fillRect((canvas.width / 2 - 85), 153, 170, 94);
+	
+	ctx.fillStyle = "white";
+	ctx.font = "60px Arial";
+	ctx.fillText("Play", (canvas.width / 2 - 62), 217);
+	
+	ctx.fillStyle = "red";
+	ctx.font = "70px Arial";
+	ctx.fillText("Agent Jump", canvas.width / 2 - 180, 90);
+	
+	ctx.fillStyle = "white";
+	ctx.font = "15px Arial";
+	ctx.fillText("High score: " + String(localStorage.getItem("highScore")), 10, 170);
+	ctx.fillText("Last run: " + String(localStorage.getItem("lastScore")), 10, 200);
+	ctx.fillText("Killed by: " + String(localStorage.getItem("killedBy")), 10, 230);
+	ctx.fillText("Current Ducks: " + String(localStorage.getItem("currentDucks")), 10, 260);
+	
+	
+	ctx.fillStyle = "white";
+	ctx.fillRect((canvas.width - 100), 170, 70, 70);
+	ctx.fillStyle = "#2b2b2b";
+	ctx.fillRect((canvas.width - 97), 173, 64, 64);
+	
+	
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText("W", canvas.width - 87, 222, 146, 70);
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	ctx.fillText("START", canvas.width - 95, 160, 146, 70);
+	
+	ctx.fillStyle = "white";
+	ctx.fillRect((canvas.width - 200), 170, 70, 70);
+	ctx.fillStyle = "#2b2b2b";
+	ctx.fillRect((canvas.width - 197), 173, 64, 64);
+	
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText("S", canvas.width - 182, 222, 146, 70);
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	ctx.fillText("STORE", canvas.width - 200, 160, 146, 70);
 }
 
 
 function hoverButtonStart() {
-	if(!storeOpen){ 
-		var canvas = document.getElementById("gameArea");
-		var ctx = canvas.getContext("2d");
-		
-		ctx.globalCompositeOperation = 'destination-under'
-		
-		ctx.fillStyle = "#525252";
-		ctx.fillRect((canvas.width / 2 - 85), 153, 170, 94);
-		
-		ctx.fillStyle = "white";
-		ctx.font = "60px Arial";
-		ctx.fillText("Play", (canvas.width / 2 - 62), 217);
-	}
+	var canvas = document.getElementById("gameArea");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.globalCompositeOperation = 'destination-under'
+	
+	ctx.fillStyle = "#525252";
+	ctx.fillRect((canvas.width / 2 - 85), 153, 170, 94);
+	
+	ctx.fillStyle = "white";
+	ctx.font = "60px Arial";
+	ctx.fillText("Play", (canvas.width / 2 - 62), 217);
 }
 
 
@@ -153,12 +150,10 @@ function hoverButtonStart() {
 
 
 function getRelativeCoordinates(event, element) {
-	if(!storeOpen){   
-		const rect = element.getBoundingClientRect();
-		const x = event.clientX - rect.left;
-		const y = event.clientY - rect.top;
-		return { x, y };
-	}
+	const rect = element.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+	return { x, y };
 }
 
 
@@ -170,7 +165,6 @@ var gameArea = {
 		this.context = this.canvas.getContext("2d");
 		$(`#gameWindow`).append(this.canvas);
 		this.interval = setInterval(updateGameArea, 25);
-		window.addEventListener('mousemove', mouseMove, true)
 		window.addEventListener('keydown', keyDown, true)
 		window.addEventListener('keyup', keyUp, true)
 	},
@@ -183,250 +177,212 @@ var gameArea = {
 }
 
 function backgroundColor(color) {
-	if(!storeOpen){     
-		var canvas = document.getElementById("gameArea");
-		var ctx = canvas.getContext("2d");
-		
-		ctx.globalCompositeOperation = 'destination-under'
-		
-		ctx.fillStyle = color;
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	}
+	var canvas = document.getElementById("gameArea");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.globalCompositeOperation = 'destination-under'
+	
+	ctx.fillStyle = color;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function updateScore() {
-	if(!storeOpen){ 
-	  var canvas = document.getElementById("gameArea");
-	  var ctx = canvas.getContext("2d");
+	var canvas = document.getElementById("gameArea");
+	var ctx = canvas.getContext("2d");
 	
-	  ctx.fillStyle = "white";
-	  ctx.font = "20px Arial";
-	  ctx.fillText("Ducks: " + String(Score), 10, 30);
-	}
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	ctx.fillText("Ducks: " + String(Score), 10, 30);
 }
 
 function failScreen() {
-	if(!storeOpen){ 
-		backgroundColor("black");
-		var canvas = document.getElementById("gameArea");
-		var ctx = canvas.getContext("2d");
-		
-		if(Score < 10){
+	var canvas = document.getElementById("gameArea");
+	var ctx = canvas.getContext("2d");
+	backgroundColor("black");
+	
+	
+	//FAIL SPLASH TITLE
+	if(Score < 10){
 		ctx.fillStyle = "red";
 		ctx.font = "70px Arial";
 		ctx.fillText("AUTOFAIL", canvas.width / 2 - 160, 100);
-		} else if(Score >= 10 && Score < 50) {
+	} else if(Score >= 10 && Score < 50) {
 		ctx.fillStyle = "orange";
 		ctx.font = "70px Arial";
 		ctx.fillText("KEEP TRYING", canvas.width / 2 - 150, 100);
-		} else if(Score >= 50 && Score < 100) {
+	} else if(Score >= 50 && Score < 100) {
 		ctx.fillStyle = "green";
 		ctx.font = "70px Arial";
 		ctx.fillText("GOOD JOB", canvas.width / 2 - 160, 100);
-		} else if(Score >= 100) {
+	} else if(Score >= 100) {
 		ctx.fillStyle = "white";
 		ctx.font = "70px Arial";
 		ctx.fillText("YOU WIN", canvas.width / 2 - 170, 100);
-		}
-		
-		ctx.fillStyle = "white";
-		ctx.font = "20px Arial";
-		if(Score == 1)
-		ctx.fillText("You collected " + String(Score) + " duck.", (canvas.width / 2 - 100), 155);
-		else
-		ctx.fillText("You collected " + String(Score) + " ducks.", (canvas.width / 2 - 100), 150);
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect((canvas.width - 100), 170, 70, 70);
-		ctx.fillStyle = "#2b2b2b";
-		ctx.fillRect((canvas.width - 97), 173, 64, 64);
-		
-		
-		ctx.fillStyle = "white";
-		ctx.font = "50px Arial";
-		ctx.fillText("M", canvas.width - 86, 222, 146, 70);
-		ctx.fillStyle = "white";
-		ctx.font = "20px Arial";
-		ctx.fillText("MENU", canvas.width - 95, 160, 146, 70);
-		
-		ctx.strokeStyle = "red";
-		ctx.lineWidth = 10;
-		
-		ctx.beginPath();
-		ctx.moveTo((canvas.width - 100), 170);
-		ctx.lineTo((canvas.width - 30), 250);
-		
-		ctx.stroke();
-		
-		ctx.beginPath();
-		ctx.moveTo((canvas.width - 100), 250);
-		ctx.lineTo((canvas.width - 30), 170);
-		
-		ctx.stroke();
-		
-		ctx.fillStyle = "red";
-		ctx.font = "10px Arial";
-		ctx.fillText("Work in Progress", canvas.width - 105, 260);
-		
-		ctx.fillStyle = "white";
-		ctx.fillRect(30, 170, 70, 70);
-		ctx.fillStyle = "#2b2b2b";
-		ctx.fillRect(33, 173, 64, 64);
-		
-		ctx.fillStyle = "white";
-		ctx.font = "50px Arial";
-		ctx.fillText("R", 45, 222, 146, 70);
-		ctx.fillStyle = "white";
-		ctx.font = "20px Arial";
-		ctx.fillText("RETRY", 33, 160, 146, 70);
 	}
+
+	//LAST SCORE OUTPUT
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	
+	if(Score == 1){
+		ctx.fillText("You collected " + String(Score) + " duck.", (canvas.width / 2 - 100), 155);
+	} else {
+		ctx.fillText("You collected " + String(Score) + " ducks.", (canvas.width / 2 - 100), 150);
+	}
+
+	//MENU BUTTON
+	ctx.fillStyle = "white";
+	ctx.fillRect((canvas.width - 100), 170, 70, 70);
+	ctx.fillStyle = "#2b2b2b";
+	ctx.fillRect((canvas.width - 97), 173, 64, 64);
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText("M", canvas.width - 86, 222, 146, 70);
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	ctx.fillText("MENU", canvas.width - 95, 160, 146, 70);
+	
+	//RETRY BUTTON
+	ctx.fillStyle = "white";
+	ctx.fillRect(30, 170, 70, 70);
+	ctx.fillStyle = "#2b2b2b";
+	ctx.fillRect(33, 173, 64, 64);
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText("R", 45, 222, 146, 70);
+	ctx.fillStyle = "white";
+	ctx.font = "20px Arial";
+	ctx.fillText("RETRY", 33, 160, 146, 70);
 }
 
 
 
 function component(width, height, color, x, y, type) {
-	if(!storeOpen){   
-		this.width = width;
-		this.height = height;
-		this.speedX = 0;
-		this.speedY = 0;
-		this.x = x;
-		this.y = y;
-		this.update = function(){
-			ctx = gameArea.context;
-			ctx.fillStyle = color;
-			ctx.fillRect(this.x, this.y, this.width, this.height);
+	this.width = width;
+	this.height = height;
+	this.speedX = 0;
+	this.speedY = 0;
+	this.x = x;
+	this.y = y;
+	this.update = function(){
+		ctx = gameArea.context;
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+	this.newPos = function() {
+		this.x += this.speedX;
+		this.y += this.speedY;
+	}
+	
+	this.jump = function(){
+		if(isOnFloor && type == "player")
+		this.speedY = -15;
+	}
+	this.floorCheck = function(){
+		if(this.y >= (gameArea.canvas.height * 0.75)){
+			this.y = (gameArea.canvas.height * 0.75);
+			isOnFloor = true;
+		} else {
+			isOnFloor = false;
 		}
-		this.newPos = function() {
-			this.x += this.speedX;
-			this.y += this.speedY;
-		}
-		
-		this.jump = function(){
-			if(isOnFloor && type == "player")
-			this.speedY = -15;
-		}
-		this.floorCheck = function(){
-			if(this.y >= (gameArea.canvas.height * 0.75)){
-				this.y = (gameArea.canvas.height * 0.75);
-				isOnFloor = true;
-			} else {
-				isOnFloor = false;
-			}
-		
-		}
-		
-		this.crashWith = function(otherobj) {
-			var myleft = this.x;
-			var myright = this.x + (this.width);
-			var mytop = this.y;
-			var mybottom = this.y + (this.height);
-			var otherleft = otherobj.x;
-			var otherright = otherobj.x + (otherobj.width);
-			var othertop = otherobj.y;
-			var otherbottom = otherobj.y + (otherobj.height);
-			var crash = true;
-			if ((mybottom < othertop) ||
-			(mytop > otherbottom) ||
-			(myright < otherleft) ||
-			(myleft > otherright)) {
-			crash = false;
-		}
-			return crash;
-		}
+	
+	}
+	
+	this.crashWith = function(otherobj) {
+		var myleft = this.x;
+		var myright = this.x + (this.width);
+		var mytop = this.y;
+		var mybottom = this.y + (this.height);
+		var otherleft = otherobj.x;
+		var otherright = otherobj.x + (otherobj.width);
+		var othertop = otherobj.y;
+		var otherbottom = otherobj.y + (otherobj.height);
+		var crash = true;
+		if ((mybottom < othertop) ||
+		(mytop > otherbottom) ||
+		(myright < otherleft) ||
+		(myleft > otherright)) {
+		crash = false;
+	}
+		return crash;
 	}
 }
 
 
 function updateGameArea() {
-	if(!storeOpen){   
-		if (!gameStart){
-			gameArea.clear();
-			titleScreen();
-			document.onmousedown = function(e) {mousePressed = true;};
-			document.onmouseup = function(e) {mousePressed = false;};
-			
-			if(mouseX > 310 && mouseX < 480){
-				if(mouseY > 203 && mouseY < 297){
-					hoverButtonStart();
-					if(mousePressed){
-						gameStart = true;
-						gameArea.clear();
-						beginSelectedGame();
-					}
-				}
-			}
-			
-		} else if (Agent.crashWith(Sup)) {  
-			gameArea.clear();
-			localStorage.setItem("lastScore", Score);
-			//localStorage.setItem("currentDucks", );
-			localStorage.setItem("killedBy", "Sup");
-			if(Score > localStorage.getItem("highScore"))
-				localStorage.setItem("highScore", Score);
-			
-			failScreen();
-			if(gameArea.key && gameArea.key == 82){
-				resetGame();
-			} else if (gameArea.key && gameArea.key == 77){
-			gameStart = false;
-				resetGame();
-			}
+	if (!gameStart){
+		gameArea.clear();
+		titleScreen();
 		
-		} else if(gameStart){
-			gameArea.clear();
-			backgroundColor("black");
-			updateScore();
-
-
-
-			//AGENT CODE
-			Agent.floorCheck();
-			gravity();
-			
-			if (Agent.crashWith(Duck)) {
-				var randX = (Math.floor(Math.random() * 500) + 300);
-				Duck.x = (gameArea.canvas.width + randX);
-				var randY = (-(Math.floor(Math.random() * 50)) - 50);
-				Duck.y = ((gameArea.canvas.height * 0.75) + randY);
-				Score += 1;
-			}
-
-			if (gameArea.key && gameArea.key == 87) {Agent.jump()};
-
-			Agent.newPos();
-			Agent.update();
-
-
-			//SUP CODE
-			Sup.x -= gameSpeed;
-			Sup.speedY = 0;
-
-			if (Sup.x < -30){
-				var rand = (Math.floor(Math.random() * 200) + 100);
-				Sup.x = (gameArea.canvas.width + rand);
-				gameSpeed += 0.05
-			}
-
-			Sup.update();
-
-
-			//DUCK CODE
-			
-			if(Duck.x < -30){
-				var randX = (Math.floor(Math.random() * 500) + 300);
-				Duck.x = (gameArea.canvas.width + randX);
-				var randY = (-(Math.floor(Math.random() * 50)) - 50);
-				Duck.y = ((gameArea.canvas.height * 0.75) + randY);
-			}
-			
-			Duck.x -= gameSpeed;
-			Duck.speedY = 0;
-			Duck.update();
-			
-			
+	} else if (Agent.crashWith(Sup)) {  
+		gameArea.clear();
+		localStorage.setItem("lastScore", Score);
+		//localStorage.setItem("currentDucks", );
+		localStorage.setItem("killedBy", "Sup");
+		if(Score > localStorage.getItem("highScore"))
+			localStorage.setItem("highScore", Score);
 		
+		failScreen();
+		if(gameArea.key && gameArea.key == 82){
+			resetGame();
+		} else if (gameArea.key && gameArea.key == 77){
+		gameStart = false;
+			resetGame();
 		}
+	
+	} else if(gameStart){
+		gameArea.clear();
+		backgroundColor("black");
+		updateScore();
+
+
+
+		//AGENT CODE
+		Agent.floorCheck();
+		gravity();
+		
+		if (Agent.crashWith(Duck)) {
+			var randX = (Math.floor(Math.random() * 500) + 300);
+			Duck.x = (gameArea.canvas.width + randX);
+			var randY = (-(Math.floor(Math.random() * 50)) - 50);
+			Duck.y = ((gameArea.canvas.height * 0.75) + randY);
+			Score += 1;
+		}
+
+		if (gameArea.key && gameArea.key == 87) {Agent.jump()};
+
+		Agent.newPos();
+		Agent.update();
+
+
+		//SUP CODE
+		Sup.x -= gameSpeed;
+		Sup.speedY = 0;
+
+		if (Sup.x < -30){
+			var rand = (Math.floor(Math.random() * 200) + 100);
+			Sup.x = (gameArea.canvas.width + rand);
+			gameSpeed += 0.05
+		}
+
+		Sup.update();
+
+
+		//DUCK CODE
+		
+		if(Duck.x < -30){
+			var randX = (Math.floor(Math.random() * 500) + 300);
+			Duck.x = (gameArea.canvas.width + randX);
+			var randY = (-(Math.floor(Math.random() * 50)) - 50);
+			Duck.y = ((gameArea.canvas.height * 0.75) + randY);
+		}
+		
+		Duck.x -= gameSpeed;
+		Duck.speedY = 0;
+		Duck.update();
+		
+		
+	
 	}
 }
 
@@ -437,7 +393,6 @@ function resetGame(){
 		clearInterval(updateGameArea);
 		Score = 0;
 		gameSpeed = 0;
-		window.removeEventListener('mousemove', mouseMove, true)
 		window.removeEventListener('keydown', keyDown, true)
 		window.removeEventListener('keyup', keyUp, true)
 		beginSelectedGame();
@@ -452,7 +407,6 @@ function resetGameMenu(){
 		Score = 0;
 		gameSpeed = 0;
 		gameStart = false;
-		window.removeEventListener('mousemove', mouseMove, true)
 		window.removeEventListener('keydown', keyDown, true)
 		window.removeEventListener('keyup', keyUp, true)
 		var oldCanv = document.getElementById('gameArea');
